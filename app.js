4049d0d7a304d6d5b4ad13762d8a632fab578c7c
Marek <github@marekurban.de>
2021-01-19 00:25:40 +0100

    Create base file structure with ESM enabled

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import routes from "./routes/index.js";
+
+const app = express();
+
+app.use("/", routes);
+
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
